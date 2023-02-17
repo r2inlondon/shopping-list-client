5bbd144d1ca26225c3b820205bdd92ea4644eef9
Arturo Quiroz <r2inlondon@gmail.com>
2023-02-17 13:35:26 +0000

    feat : api call to find user

--- src/componets/LoginComp.jsx
+++ src/componets/LoginComp.jsx
@@ -1,12 +1,11 @@
 import { useState, Fragment } from "react";
 import { validateEmail } from "../utils/validateLogin";
+import { checkLogin } from "../api/api-calls.js";
 
 const LoginComp = () => {
-  // const [email, setEmail] = useState("");
-  // const [pass, setPass] = useState("");
   const [userCred, setUserCred] = useState({
     email: "",
-    Pass: "",
+    pass: "",
   });
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -27,41 +26,55 @@ const LoginComp = () => {
     }
   };
 
-  const handlePass = (e) => {};
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const isValid = await checkLogin(userCred);
+
+    console.log({ isValid });
   };
 
   return (
-    <form className="w-64" onSubmit={handleSubmit}>
-      <label className="block text-md font-medium text-gray-700">
-        Email Address
-      </label>
-      {errorMessage && <p className="text-sm text-pink-600">{errorMessage}</p>}
-      <input
-        className="my-4 block w-full rounded-md border-gray-500 shadow-md focus:border-indigo-500 focus:ring-indigo-500 sm:text-md"
-        type="text"
-        value={userCred.email}
-        onChange={handleEmail}
-      />
-
-      <label className="block text-sm font-medium text-gray-700">
-        Password
+    <Fragment>
+      <form className="w-64" onSubmit={handleSubmit}>
+        <label className="block text-md font-medium text-gray-700">
+          Email Address
+        </label>
+        {errorMessage && (
+          <p className="text-sm text-pink-600">{errorMessage}</p>
+        )}
         <input
           className="my-4 block w-full rounded-md border-gray-500 shadow-md focus:border-indigo-500 focus:ring-indigo-500 sm:text-md"
           type="text"
-          value={userCred.pass}
-          onChange={(e) => setPass(e.target.value)}
+          value={userCred.email}
+          onChange={handleEmail}
+          autoComplete="new-password"
         />
-      </label>
-      <button
-        type="submit"
-        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        Login
-      </button>
-    </form>
+
+        <label className="block text-sm font-medium text-gray-700">
+          Password
+          <input
+            className="my-4 block w-full rounded-md border-gray-500 shadow-md focus:border-indigo-500 focus:ring-indigo-500 sm:text-md"
+            type="password"
+            autoComplete="new-password"
+            value={userCred.pass}
+            onChange={(e) => {
+              setUserCred((prevState) => ({
+                ...prevState,
+                pass: e.target.value,
+              }));
+            }}
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="inline-flex justify-center rounded-md border border-transparent bg-green-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+        >
+          Login
+        </button>
+      </form>
+    </Fragment>
   );
 };
 
