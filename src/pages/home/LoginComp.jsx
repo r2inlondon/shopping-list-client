b4edf9d6e6e0999e88f47fc0e1adca8d2b828afb
Arturo Quiroz <r2inlondon@gmail.com>
2023-02-28 18:48:12 +0000

    feat : Login and Authentication with Axios part - 1 completed

--- src/pages/home/LoginComp.jsx
+++ src/pages/home/LoginComp.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState, useContext } from "react";
 import { validateEmail } from "../../utils/validateLogin";
 import AuthContext from "../../context/AuthProvider";
@@ -33,10 +33,18 @@ const LoginComp = () => {
           withCredentials: true,
         }
       );
+      const accessToken = response?.data?.accessToken;
+      setAuth({ email, password, accessToken });
       setErrMsg("");
-      return navigate("/userDashboard");
+      navigate("/userDashboard");
     } catch (err) {
-      setErrMsg(err.message);
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      }
+      if (err.response?.status === 400) {
+        setErrMsg("Login Failed");
+      }
+      return;
     }
   };
 
@@ -73,7 +81,7 @@ const LoginComp = () => {
           <input
             className="my-4 block w-full rounded-md border-gray-500 shadow-md focus:border-indigo-500 focus:ring-indigo-500 sm:text-md"
             type="password"
-            // autoComplete="new-password"
+            autoComplete="new-password"
             value={userCred.password}
             onChange={(e) => {
               setUserCred((prevState) => ({
@@ -86,18 +94,18 @@ const LoginComp = () => {
         <div className="flex justify-between">
           <button
             type="submit"
-            className="inline-flex justify-center rounded-md border border-transparent bg-green-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+            className="w-full inline-flex justify-center rounded-md border border-transparent bg-green-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
           >
             Login
           </button>
-          <button
-            onClick={() => navigate("/register")}
-            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Sign Up
-          </button>
         </div>
       </form>
+      <p className="my-4">
+        Need an account?{" "}
+        <Link to="/register">
+          <span className="text-blue-600">SignUp</span>
+        </Link>
+      </p>
     </div>
   );
 };
