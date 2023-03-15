5932ed631793b2f0bcb16f00307d66ca2a69e618
Arturo Quiroz <r2inlondon@gmail.com>
2023-03-15 21:40:02 +0000

    feat : implemented Flipper and Flipped, animation is better now

--- src/pages/shopping/ShoppingItemsPage.jsx
+++ src/pages/shopping/ShoppingItemsPage.jsx
@@ -4,6 +4,7 @@ import { useEffect, Fragment, useState, useRef } from "react";
 import AddProductForm from "../../components/AddProductForm";
 import ReModal from "../../components/ReModal";
 import sortBy from "sort-by";
+import { Flipper, Flipped } from "react-flip-toolkit";
 
 const ShoppingItemsPage = () => {
   let { listId } = useParams();
@@ -14,11 +15,14 @@ const ShoppingItemsPage = () => {
   const [showModal, setShowModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [errMsg, setErrMsg] = useState("");
-  const [selectedItem, setSelectedItem] = useState(null);
   const noCheckedStyle =
-    "flex justify-between items-center py-2 mb-2 bg-lime-100";
+    "flex justify-between items-center h-8 mb-2 bg-lime-100";
+  // const transitionStyle =
+  //   "transform translate-y-28 opacity-0 ease-linear duration-1000";
   const checkedStyle =
-    "translate-y-6 opacity-0 transition-opacity duration-500 ease-in-out";
+    " opacity-50 flex justify-between items-center py-2 mb-2 bg-gray-100";
+  const textAndCheckboxSytle =
+    "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600";
 
   useEffect(() => {
     let isMounted = true;
@@ -70,22 +74,17 @@ const ShoppingItemsPage = () => {
     setShowModal(false);
   };
 
-  const handleCompleted = (index) => {
-    setSelectedItem(index);
-  };
-
-  const handleTransition = async (e) => {
-    const { id, checked } = e.target.children[1];
-    console.log(id, checked);
+  const handleCompleted = async (itemId, checked) => {
+    const newItems = listItems.filter((item) => item.id !== itemId);
+    setListItems(newItems);
 
     try {
       await axiosPrivate.put("/shopping", {
         headers: { "Content-Type": "application/json" },
         withCredentials: true,
-        id,
+        id: itemId,
         completed: !checked,
       });
-      setSelectedItem(null);
       setIsLoading(true);
     } catch (err) {
       console.error(err.message);
@@ -109,18 +108,21 @@ const ShoppingItemsPage = () => {
     <Fragment>
       <div className="flex justify-between mb-4">
         <button
+          tabIndex={1}
           onClick={() => navigate("/home/ListPage/")}
           className="inline-flex justify-center border border-transparent bg-slate-300 py-2 px-4 text-sm font-medium text-black shadow-sm hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
         >
           Home
         </button>
         <button
+          tabIndex={2}
           onClick={() => deleteCompleted()}
           className="inline-flex justify-center border border-transparent bg-slate-300 py-2 px-4 text-sm font-medium text-black shadow-sm hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
         >
           Clear
         </button>
         <button
+          tabIndex={3}
           onClick={() => setShowModal(true)}
           className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
         >
@@ -135,30 +137,43 @@ const ShoppingItemsPage = () => {
           {errMsg}
         </p>
       )}
-      {listItems?.length ? (
-        listItems.map((item, index) => (
-          <div
-            onTransitionEnd={(e) => handleTransition(e)}
-            key={item.id}
-            className={selectedItem === index ? checkedStyle : noCheckedStyle}
-            // className={`${noCheckedStyle} ${
-            //   selectedItem === index ? "translate-y-6 opacity-0" : "opacity-100"
-            // } transition-opacity duration-500 ease-in-out`}
-          >
-            <span>{item.product.name}</span>
-            <input
-              id={item.id}
-              type="checkbox"
-              value={item.id}
-              checked={item.completed}
-              onChange={() => handleCompleted(index)}
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />
-          </div>
-        ))
-      ) : (
-        <h3>Shopping List is empty</h3>
-      )}
+      <Flipper flipKey={listItems.length} spring="wobbly">
+        {listItems.length > 0 &&
+          listItems.map((item, index) => {
+            if (!item.completed) {
+              return (
+                <Flipped key={item.id} flipId={item.id}>
+                  <div className={noCheckedStyle}>
+                    <span>{item.product.name}</span>
+                    <input
+                      id={item.id}
+                      type="checkbox"
+                      value={item.id}
+                      checked={item.completed}
+                      onChange={() => handleCompleted(item.id, item.completed)}
+                      className={textAndCheckboxSytle}
+                    />
+                  </div>
+                </Flipped>
+              );
+            } else {
+              return (
+                <div key={item.id} className={checkedStyle}>
+                  <span>{item.product.name}</span>
+                  <input
+                    id={item.id}
+                    type="checkbox"
+                    value={item.id}
+                    checked={item.completed}
+                    onChange={() => handleCompleted(item.id, item.completed)}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                </div>
+              );
+            }
+          })}
+      </Flipper>
+      {listItems.length === 0 && <h3>Shopping List is empty</h3>}
       <ReModal showModal={showModal}>
         <AddProductForm addProduct={addProduct} setShowModal={setShowModal} />
       </ReModal>
