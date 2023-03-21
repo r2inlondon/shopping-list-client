e553f8046b3206df497aba5b2ab0a097ec2107af
arturo quiroz <r2inlondon@gmail.com>
2023-03-21 11:08:18 +0000

    fix : replaced BrowserRouter for HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthProvider";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 if (import.meta.env.VITE_NODE_ENV === "production") {
@@ -12,12 +12,12 @@ if (import.meta.env.VITE_NODE_ENV === "production") {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
