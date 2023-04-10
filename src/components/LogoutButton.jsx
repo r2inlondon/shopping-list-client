ab6678022af66fa211787514a8e23fe18f50d20e
Arturo Quiroz <r2inlondon@gmail.com>
2023-04-10 10:48:08 +0100

    style : logout button absoluted position

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -18,7 +18,7 @@ const LogoutButton = () => {
   };
 
   return (
-    <div className="flex justify-end">
+    <div className="flex justify-end absolute bottom-10 right-0">
       <button
         onClick={logout}
         className="w-28 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-600 text-base font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:text-sm"
--- src/layouts/UserHome.jsx
+++ src/layouts/UserHome.jsx
@@ -3,8 +3,8 @@ import { Fragment } from "react";
 
 const UserHome = () => (
   <Fragment>
-    <div className="w-screen">
-      <div className="w-11/12 m-auto">
+    <div className="w-screen h-screen">
+      <div className="w-11/12 h-full m-auto relative">
         <div className="h-24 m-auto bg-gray-400">
           <h1>Shopping List App</h1>
         </div>
