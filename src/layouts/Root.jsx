33bca18480999463147df8241505b7a29e4f3cb3
Arturo Quiroz <r2inlondon@gmail.com>
2023-04-16 18:29:09 +0100

    style : bg-image and app width styled for medium or big screens

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <main className="h-screen bg-[url('src/assets/shoppingList-bg.jpg')] bg-cover">
-      <Outlet />
+    <main className="h-screen bg-black">
+      <div className="max-w-xl h-screen m-auto bg-[url('src/assets/shoppingList-bg.jpg')] bg-center bg-no-repeat md:rounded-md">
+        <Outlet />
+      </div>
     </main>
   );
 };
