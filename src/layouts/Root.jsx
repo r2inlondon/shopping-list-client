b0ad81d024a7776eebb578b206574473b27324fe
arturo quiroz <r2inlondon@gmail.com>
2023-04-20 15:20:48 +0100

    style : fixed the view on big screens

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -3,9 +3,9 @@ import HeaderComp from "../components/HeaderComp";
 
 const Root = () => {
   return (
-    <main className="h-screen bg-black">
-      <div className="max-w-xl h-screen m-auto bg-[url('src/assets/shoppingList-bg.jpg')] bg-center bg-no-repeat md:rounded-md ">
-        <div className="w-11/12 h-screen md:h-95  m-auto relative">
+    <main className="h-screen bg-black md:flex items-center justify-center">
+      <div className="h-full sm:w-[645px] md:h-5/6 bg-[url('src/assets/shoppingList-bg.jpg')] bg-center bg-no-repeat md:rounded-md ">
+        <div className="w-11/12 h-full m-auto relative">
           <div className="h-1/6 md:h-1/5 mb-4 md:mb-6 flex justify-center items-end">
             <HeaderComp />
           </div>
