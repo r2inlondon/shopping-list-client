5ae3966629a6f113ee17f684dbdd32ced625eb42
Arturo Quiroz <r2inlondon@gmail.com>
2023-04-20 23:15:22 +0100

    style : AddListForm has been styled

--- src/pages/listsPage/AddListForm.jsx
+++ src/pages/listsPage/AddListForm.jsx
@@ -41,7 +41,7 @@ const AddListForm = ({
         <div className="px-4 pt-4 sm:p-6 sm:pb-4">
           <div className="sm:flex sm:items-start">
             <input
-              className="border border-gray-300 rounded-md p-2 w-full"
+              className="border border-gray-300 rounded-md p-2 w-full text-sm md:text-lg"
               type="text"
               placeholder="Enter list name"
               required
@@ -54,13 +54,13 @@ const AddListForm = ({
         <div className="mx-4 sm:mx-0 my-4 sm:px-6 sm:flex sm:flex-row-reverse">
           <button
             type="submit"
-            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 mb-4 bg-btn-color text-base font-medium text-white hover:bg-btn-color-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:text-sm transition-colors duration-200"
+            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 mb-4 bg-btn-color text-sm md:text-lg font-medium text-white hover:bg-btn-color-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:text-sm transition-colors duration-200"
           >
             Submit
           </button>
           <button
             type="button"
-            className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 mb-4 bg-white text-base font-medium text-red-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white sm:ml-3 sm:text-sm transition-colors duration-200"
+            className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 mb-4 bg-white text-sm md:text-lg font-medium text-red-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white  sm:text-sm transition-colors duration-200"
             onClick={() => handleCancelModal()}
           >
             Cancel
