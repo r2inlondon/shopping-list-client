6909982867734159499120993899330b5d59b441
arturo quiroz <r2inlondon@gmail.com>
2023-04-27 16:53:53 +0100

    style : fixed modal borders mobile view

--- src/pages/shoppingItemsPage/AddProductForm.jsx
+++ src/pages/shoppingItemsPage/AddProductForm.jsx
@@ -73,7 +73,7 @@ const AddProductForm = ({ addProduct, setShowModal }) => {
   };
 
   return (
-    <div className="h-56 w-full">
+    <div className="h-fit w-full">
       {submitting && (
         <div
           role="status"
@@ -85,7 +85,7 @@ const AddProductForm = ({ addProduct, setShowModal }) => {
       {errMsg && <ErrorMessage errMsg={errMsg} setErrMsg={setErrMsg} />}
       {!submitting && (
         <form onSubmit={handleSubmit}>
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div className="px-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
               <div className="w-full mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <div className="mt-2">
@@ -102,7 +102,7 @@ const AddProductForm = ({ addProduct, setShowModal }) => {
               </div>
             </div>
           </div>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <button
               type="submit"
               className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:mt-0 sm:ml-3 sm:text-sm"
