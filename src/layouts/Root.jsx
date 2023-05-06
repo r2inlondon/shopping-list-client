869ffe52fbb17f7b6b1c49a9ebb3ed383da024e3
Arturo Quiroz <r2inlondon@gmail.com>
2023-05-06 15:09:31 +0100

    refactor and style : opacity added to the background image and styled to hide when mobile view is on

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -3,15 +3,16 @@ import HeaderComp from "../components/HeaderComp";
 
 const Root = () => {
   return (
-    <main className="h-screen bg-[url('src/assets/health_food.webp')] bg-center md:flex items-center justify-center">
-      <div className="h-full bg-white sm:w-[645px] md:h-5/6 md:rounded-md shadow-md ">
+    <main className="h-screen relative md:flex items-center justify-center">
+      <div className="h-full bg-white sm:w-[645px] md:h-5/6 md:rounded-md shadow-md z-10">
         <div className="w-11/12 h-full m-auto relative">
-          <div className="h-1/6 md:h-1/5 mb-4 md:mb-6 flex justify-center items-end">
+          <div className="h-1/6 md:h-1/5 mb-4 md:mb-6 flex justify-center items-end z-20">
             <HeaderComp />
           </div>
           <Outlet />
         </div>
       </div>
+      <div className="absolute inset-0 sm:bg-[url('src/assets/shopping-bg.png')] sm:opacity-50 -z-10 sm:z-0"></div>
     </main>
   );
 };
