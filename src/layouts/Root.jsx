a90512c7b037cca7e1fbd7e00b029a981ecb1ba2
Arturo Quiroz <r2inlondon@gmail.com>
2023-05-11 22:27:30 +0100

    fix : updated path to bg image in root compoment

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -3,16 +3,16 @@ import HeaderComp from "../components/HeaderComp";
 
 const Root = () => {
   return (
-    <main className="h-screen relative md:flex items-center justify-center">
-      <div className="h-full bg-white sm:w-[645px] md:h-5/6 md:rounded-md shadow-md z-10">
-        <div className="w-11/12 h-full m-auto relative">
-          <div className="h-1/6 md:h-1/5  flex justify-center items-end z-20">
+    <main className="relative h-screen items-center justify-center md:flex">
+      <div className="z-10 h-full bg-white shadow-md sm:w-[645px] md:h-5/6 md:rounded-md">
+        <div className="relative m-auto h-full w-11/12">
+          <div className="z-20 flex h-1/6 items-end justify-center md:h-1/5">
             <HeaderComp />
           </div>
           <Outlet />
         </div>
       </div>
-      <div className="absolute inset-0 sm:bg-[url('src/assets/shopping-bg.png')] sm:opacity-50 -z-10 sm:z-0"></div>
+      <div className="absolute inset-0 -z-10 sm:z-0 sm:bg-[url('assets/shopping-bg.png')] sm:opacity-50"></div>
     </main>
   );
 };
